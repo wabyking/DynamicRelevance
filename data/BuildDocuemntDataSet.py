14b220b6ef40b9f27c7d64c394704d86bfe2555a
wabyking <810553967@qq.com>
2015-12-24 10:53:57 +0800

    jieba  to tokenizer the Chinese text  and add the title

--- data/BuildDocuemntDataSet.py
+++ data/BuildDocuemntDataSet.py
@@ -2,6 +2,7 @@ import json
 import pymongo
 from xml.dom.minidom import parse
 import xml.dom.minidom
+import jieba
 class DateEncoder(json.JSONEncoder ):  
     def default(self, obj):  
         if isinstance(obj,dict):  
@@ -70,6 +71,33 @@ def getinfo(collection_name):
 	db=client.test
 	for doc in db[collection_name].find():
 		print doc
+def check_contain_chinese(check_str):
+	count=0
+	for ch in check_str:
+		if u'\u4e00' <= ch <= u'\u9fff':
+			count+=1
+	if count*0.1/len(check_str) >0.3:
+		return True
+	return False
+def addLabel(text): 
+	if check_contain_chinese(text):
+		tokens=jieba.cut(text)
+		labeledText=""
+		for token in tokens:
+			labeledText +="<a>"+token+"</a>"
+		return labeledText
+	else:
+		tokens=text.split()
+		labeledText=""
+		for token in tokens:
+			labeledText +="<a>"+token+" </a>"
+		return labeledText
+
+
+
+		
+	
+
 
 def dataFromXml(filename):
 	DOMTree = xml.dom.minidom.parse( filename )
@@ -84,10 +112,13 @@ def dataFromXml(filename):
 		desicription=record.getElementsByTagName("desicription") [0].childNodes[0].data
 		document1=record.getElementsByTagName("d1") [0].childNodes[0].data
 		document2=record.getElementsByTagName("d2") [0].childNodes[0].data
-		data={"id":id,"query":query,"discription":desicription,"document1":document1,"document2":document2}
-		print desicription
+		title1=record.getElementsByTagName("d1") [0].getAttribute("title")
+		title2=record.getElementsByTagName("d2") [0].getAttribute("title")
+		data={"id":id,"query":query,"discription":addLabel(desicription),"title1":title1,"document1":addLabel(document1),"title2":title2,"document2":addLabel(document2)}
+		#print desicription
 		datas.append(data)
 	return datas
+
 
 
 if __name__=="__main__":
@@ -96,4 +127,4 @@ if __name__=="__main__":
 	for f in files:
 		datas=dataFromXml(f)
 		appDatas(datas);
-	getinfo("docs")
+	#getinfo("docs")
